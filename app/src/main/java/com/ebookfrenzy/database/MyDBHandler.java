b82c49623bff6058d93ee789e15ff4eb3a0da993
Cory Alford <calford4@my.westga.edu>
2016-02-21 01:14:35 -0800

    Added add, find, and delete methods for the MyDBHandler

--- app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
+++ app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
@@ -1,6 +1,8 @@
 package com.ebookfrenzy.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -19,6 +21,62 @@ public class MyDBHandler extends SQLiteOpenHelper {
 
     public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    public void addProduct(Product product) {
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.getProductName());
+        values.put(COLUMN_QUANTITY, product.getQuantity());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    public Product findProduct(String productname) {
+        String query = "Select * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =  \"" + productname + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        Product product = new Product();
+
+        if (cursor.moveToFirst()) {
+            cursor.moveToFirst();
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            product.setProductName(cursor.getString(1));
+            product.setQuantity(Integer.parseInt(cursor.getString(2)));
+            cursor.close();
+        } else {
+            product = null;
+        }
+        db.close();
+        return product;
+    }
+
+    public boolean deleteProduct(String productname) {
+
+        boolean result = false;
+
+        String query = "Select * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =  \"" + productname + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        Product product = new Product();
+
+        if (cursor.moveToFirst()) {
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?", new String[] { String.valueOf(product.getID()) });
+            cursor.close();
+            result = true;
+        }
+        db.close();
+        return result;
     }
 
     @Override
